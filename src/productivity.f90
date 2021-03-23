79953d3d4c6a77b938fb6330f3d0e31f4100809a
Gabriela Sophia <gabimsophia0@gmail.com>
2021-03-23 14:29:23 -0300

    fixing f1 in funcs with 3 cleaf

--- src/productivity.f90
+++ src/productivity.f90
@@ -145,7 +145,7 @@ contains
 ! rate (molCO2/m2/s)
 
     call photosynthesis_rate(catm,temp,p0,ipar,light_limit,c4_int,n2cl,&
-         & p2cl,cl1_prod,tleaf,f1a,vm_out,jl_out)
+         & p2cl,cl1_prod(:),tleaf,f1a,vm_out,jl_out)
 
 
     ! VPD
