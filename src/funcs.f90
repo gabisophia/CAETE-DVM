3d0c2593f0d052f34749abea9f8c31e854c09da6
Gabriela Sophia <gabimsophia0@gmail.com>
2021-03-25 10:41:23 -0300

    putting k(xylem conductance) - not running

--- src/funcs.f90
+++ src/funcs.f90
@@ -35,6 +35,7 @@ module photo
         conductivity_xylemleaf ,& ! (f), Maximum xylem conductivity per unit leaf area (kg m-1 s-1 Mpa-1)
         conductance_xylemax    ,& ! (f), Maximum xylem conductance per unit leaf area (mol m-2 s-1 Mpa-1)
         xylem_waterpotential   ,& ! (f), Xylem water potential (MPa)
+        xylem_conductance      ,& ! (f), Hydraulic conductance of xylem (mol m-2 s-1 Mpa-1)
         water_stress_modifier  ,& ! (f), F5 - water stress modifier (dimensionless)
         photosynthesis_rate    ,& ! (s), leaf level CO2 assimilation rate (molCO2 m-2 s-1)
         canopy_resistence      ,& ! (f), Canopy resistence (from Medlyn et al. 2011a) (s/m) == m s-1
@@ -299,6 +300,29 @@ contains
       psi_xylem  = psi_soil - psi_g - (e_in/krc_max)
   
    end function xylem_waterpotential
+
+   !=================================================================
+   !=================================================================
+
+   function xylem_conductance(krc_max,psi_xylem,psi_50) result(k)  
+      !Xylem conductance (molm-2s-1MPa-1)
+      !Based in Manzoni et al., 2013
+      use types
+  
+      real(r_8), intent(in) :: krc_max                !molm-2s-1Mpa-1 
+      real(r_8), intent(in) :: psi_xylem              !MPa
+      real(r_8), intent(in) :: psi_50                 !MPa
+      real(r_8) :: k                                  !molm-2s-1MPa-1
+
+      real(r_8) :: stem_slope     !MPa-1 - Slope of the linear portion of the xylem vulnerability function
+      real(r_8) :: a              !vulnerability curve
+      
+      stem_slope = 65.15*(-psi_50)**(-1.25)
+      a = -4*stem_slope/100*psi_50
+
+      k = krc_max*(1+(psi_xylem/psi_50)**a)**(-1) 
+
+   end function xylem_conductance
 
    !=================================================================
    !=================================================================
