9dde303a88a0d5d12aaf4864e54fdddd28ae1ea2
Gabriela Sophia <gabimsophia0@gmail.com>
2021-03-26 10:02:53 -0300

    putting alloc

--- src/funcs.f90
+++ src/funcs.f90
@@ -47,6 +47,7 @@ module photo
         pft_area_frac          ,& ! (s), area fraction by biomass
         water_ue               ,&
         leap
+        pls_allometry
 
 contains
 
@@ -1143,6 +1144,57 @@ contains
       endif
 
    end subroutine pft_area_frac
+
+   !====================================================================
+   !====================================================================
+
+   subroutine pls_allometry (dwood1, cleaf1, cfroot1, cawood1, awood, height, diameter,&
+      &crown_area)
+
+      use types 
+      use global_par
+      use allometry_par
+
+
+      integer(i_4),parameter :: npft = npls ! plss futuramente serao
+      real(r_8),dimension(npft),intent(in) :: cleaf1, cfroot1, cawood1, awood, dwood1
+      real(r_8),dimension(npft),intent(out) :: height, diameter, crown_area
+      real(r_8),dimension(npft) :: cleaf, cawood, cfroot, dwood
+      integer(i_4) :: p
+
+
+      ! ============================
+      dwood = dwood1
+      cleaf = cleaf1
+      cfroot = cfroot1
+      cawood = cawood1
+      ! ============================
+
+      do p = 1, npft !INICIALIZE OUTPUTS VARIABLES
+         height(p) = 0.0D0
+         diameter(p) = 0.0D0
+         crown_area(p) = 0.0D0
+      enddo
+
+      !PLS DIAMETER (in m.)
+      do p = 1, npft !to grasses
+         if(awood(p) .le. 0.0D0) then
+            height(p) = 0.0D0
+            diameter(p) = 0.0D0
+            crown_area(p) = 0.0D0
+            dwood(p) = 0.0D0
+         else
+            diameter(p) = (4*(cawood(p)*1.0D3)/(dwood(p)*1D7)*pi*k_allom2)&
+            &**(1/(2+k_allom3))
+
+            height(p) = k_allom2*(diameter(p)**k_allom3)
+
+            crown_area(p) = k_allom1*(diameter(p)**krp)
+         endif
+         print*, 'height', height(p), p, 'cawood', cawood(p)
+      enddo
+
+   end subroutine pls_allometry
 
    !====================================================================
    !====================================================================
