3dd2e275a1ee5187c46f57b01d5bac934847b4d8
Gabriela Sophia <gabimsophia0@gmail.com>
2021-04-14 13:41:03 -0300

    caete.py new mask - ok

--- src/caete.py
+++ src/caete.py
@@ -47,7 +47,15 @@ print(f"RUNNING CAETÊ with {gp.npls} Plant Life Strategies")
 # GLOBAL
 out_ext = ".pkz"
 npls = gp.npls
-mask = np.load("../input/mask/mask_raisg-360-720.npy")
+
+while True:
+    maskp = input("TWO MASK OPTIONS: AMAZON BIOME (a); PAN-AMAZON (b): ")
+    if maskp == 'b':
+        mask = np.load("../input/mask/mask_raisg-360-720.npy")
+        break
+    if maskp == 'a':
+        mask = np.load("../input/mask/mask_BIOMA.npy")
+        break
 
 Pan_Amazon_RECTANGLE = "y = 160:221 x = 201:272"
 
